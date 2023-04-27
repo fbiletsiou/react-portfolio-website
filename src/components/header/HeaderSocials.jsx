250f2b45847805eef09ed47cd330536747f41af2
fbiletsiou <florabiletsiou@gmail.com>
2023-04-28 00:46:14 +0100

    Header links added

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,9 +5,9 @@ import {BsLinkedin, BsGithub, BsTwitter} from 'react-icons/bs'
 function HeaderSocials() {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com' target='_blank'><BsLinkedin/></a>
-        <a href='https://github.com' target='_blank'><BsGithub/></a>
-        <a href='https://twitter.com' target='_blank'><BsTwitter/></a>
+        <a href='https://www.linkedin.com/in/florina-biletsiou/' target='_blank'><BsLinkedin/></a>
+        <a href='https://github.com/fbiletsiou' target='_blank'><BsGithub/></a>
+        <a href='https://twitter.com/fbiletsiou' target='_blank'><BsTwitter/></a>
 
     </div>
   )
