21b44e87bc8945946ffa54baa010270ace6a6aa0
fbiletsiou <florabiletsiou@gmail.com>
2023-07-11 00:24:42 +0100

    contact email done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,9 +3,29 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
-
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+  
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_l9c84zv', 'template_fvyesgm', form.current, 'Z9Diu9ZNvqPu_0S5X')
+
+
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+      e.target.reset()
+  };
+
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -34,7 +54,7 @@ const Contact = () => {
 
         </div>
         {/* END OF CONTACT */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your message' required></textarea>
